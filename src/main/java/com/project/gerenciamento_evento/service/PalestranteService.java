0220225273283e875b86caf083b94d683d3c444e
jgnfps <cupreguiado@gmail.com>
2025-07-23 09:47:33 -0300

    feat: add findById e update

--- src/main/java/com/project/gerenciamento_evento/service/PalestranteService.java
+++ src/main/java/com/project/gerenciamento_evento/service/PalestranteService.java
@@ -3,6 +3,7 @@ package com.project.gerenciamento_evento.service;
 import com.project.gerenciamento_evento.dto.PalestranteDTO;
 import com.project.gerenciamento_evento.entity.Palestrante;
 import com.project.gerenciamento_evento.repository.PalestranteRepository;
+import jakarta.persistence.EntityNotFoundException;
 import lombok.AllArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -29,5 +30,20 @@ public class PalestranteService {
 
     public void delete(Long id) {
         palestranteRepository.deleteById(id);
+    }
+
+    public Palestrante findById(Long id) {
+        return palestranteRepository.findById(id)
+                .orElseThrow(()-> new EntityNotFoundException("Palestrante não Encontrado com o ID: " + id));
+    }
+
+    public Palestrante update(Long id, PalestranteDTO dto) {
+        Palestrante palestrante = palestranteRepository.findById(id)
+                .orElseThrow(()-> new EntityNotFoundException("Palestrante não encontrado com o ID: " + id));
+
+        palestrante.setNome(dto.nome());
+        palestrante.setBiografia(dto.biografia());
+
+        return palestranteRepository.save(palestrante);
     }
 }
