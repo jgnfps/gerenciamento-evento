7e00cf41e49041a2a11c64d5e95ad27c8ba1e413
jgnfps <cupreguiado@gmail.com>
2025-07-23 09:46:02 -0300

    feat:  feat função de buscar por Id

--- src/main/java/com/project/gerenciamento_evento/repository/InscricaoRepository.java
+++ src/main/java/com/project/gerenciamento_evento/repository/InscricaoRepository.java
@@ -4,4 +4,5 @@ import com.project.gerenciamento_evento.entity.Inscricao;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface InscricaoRepository extends JpaRepository<Inscricao, Long> {
+    boolean existsByParticipanteId(Long participanteId);
 }
